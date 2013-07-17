ae551794ab8a36a0ab6e77ad7f5820b4c291081d
Jason Campbell <jason@js.la>
2013-07-17 11:49:58 -0700

    Adds new pending tests for res-template

---
+++ test/templates/test-res-template.js
@@ -0,0 +1,14 @@
+
+describe('res.tempate = beardo(req, res, options)', function(){
+  it('successfully renders content')
+
+  it('does not override pre-existing headers')
+
+  it('optionally sets res.statusCode')
+
+  describe('cache headers and responses', function(){
+    it('hanldes if-none-match request headers')
+
+    it('does NOT 304 changed content')
+  })
+})
