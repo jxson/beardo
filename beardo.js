d34e517f72dc5f9cff38117d403de7c20fedff17
Jason Campbell <jasoncampbell@google.com>
2014-12-10 00:23:23 -0800

    Initial support for fs reads

--- beardo.js
+++ beardo.js
@@ -1,24 +1,42 @@
 
 var hogan = require('hogan.js')
 var debug = require('debug')('beardo')
+var cache = require('async-cache')
+var path = require('path')
+var fs = require('graceful-fs')
+var crypto = require('crypto')
 
 module.exports = Beardo
 
-function Beardo(options) {
-  if (!(this instanceof Beardo)) return new Beardo(options)
+function Beardo(basedir) {
+  if (!(this instanceof Beardo)) return new Beardo(basedir)
+
+  if (! basedir) {
+    throw new Error('basedir is required.')
+  }
 
   var beardo = this
 
   beardo.templates = {}
+  beardo.basedir = path.resolve(basedir)
+  beardo.files = cache({
+    load: read
+  })
 
-  // if (typeof options === 'string') {
-  //   options = { basedir: options }
-  // }
-  //
-  // var template = this
-  //
-  // template.basedir = path.resolve(options.basedir)
-  // template.collection = {}
+  function read(key, callback) {
+    debug('prime cache for %s', key)
+
+    var json = JSON.parse(key)
+
+    fs.readFile(json.file, 'utf8', callback)
+  }
+}
+
+Beardo.prototype.resolve = function(key) {
+  var beardo = this
+  var file = path.resolve(beardo.basedir, key)
+
+  return file
 }
 
 Beardo.prototype.set = function (key, value) {
@@ -28,7 +46,8 @@ Beardo.prototype.set = function (key, value) {
 
   // allows chaining like: t.set(k, v).render(ctx, cb)
   return {
-    render: beardo.render.bind(beardo, key)
+    render: beardo.render.bind(beardo, key),
+    get: beardo.get.bind(beardo, key)
   }
 }
 
@@ -36,15 +55,10 @@ Beardo.prototype.has = function(key) {
   return !! this.templates[key]
 }
 
-Beardo.prototype.get = function(key, context, callback) {
+Beardo.prototype.get = function(key, callback) {
   var beardo = this
 
   debug('get: %s', key)
-
-  if (typeof context === 'function') {
-    callback = context
-    context = {}
-  }
 
   if (beardo.has(key)) {
     debug('has %s', key)
@@ -57,7 +71,7 @@ Beardo.prototype.get = function(key, context, callback) {
       callback(null, template)
     }
   } else {
-    debug('need to read from fs %s', key)
+    beardo.read(key, callback)
   }
 }
 
@@ -66,11 +80,59 @@ Beardo.prototype.render = function(key, context, callback) {
 
   debug('render: %s', key)
 
+  if (typeof context === 'function') {
+    callback = context
+    context = {}
+  }
+
+  callback = callback.bind(beardo)
+
+
+
   beardo.get(key, function(err, template, partials) {
     if (err) return callback(err)
 
     var output = template.render(context, partials)
 
     callback(err, output)
+  })
+}
+
+Beardo.prototype.read = function(key, callback) {
+  var beardo = this
+  var file = beardo.resolve(key)
+
+  debug('needs data from fs: %s', file)
+
+  fs.stat(file, function(err, stats) {
+    if (err) return callback(err)
+
+    var index = hash(file, stats)
+
+    beardo.files.get(index, function(err, data) {
+      if (err) return callback(err)
+
+      // // This should be nicer yeah?
+      // if (err && err.code === 'ENOENT') err.name = 'Template Not Found'
+      // if (err) return callback(err)
+
+      debug('loaded from cache: %s', key)
+
+      beardo.set(key, data)
+      beardo.get(key, callback)
+    })
+  })
+}
+
+function hash(file, stats) {
+  var h = crypto.createHash('md5')
+
+  h.update(stats.ino.toString())
+  h.update(stats.mtime.toString())
+  h.update(stats.size.toString())
+
+  return JSON.stringify({
+    file: file,
+    etag: h.digest('hex')
   })
 }
--- test/test-beardo.js
+++ test/test-beardo.js
@@ -19,3 +19,31 @@ test('b.set(key, value)', function(t) {
     t.end()
   })
 })
+
+test('b.get(key, value)', function(t) {
+  beardo(dir)
+  .set('test', 'Follow @{{ username }}')
+  .get(function(err, template) {
+    t.error(err)
+    t.equal(template.render({ username: 'jxson' }), 'Follow @jxson')
+    t.end()
+  })
+})
+
+test('b.render(name, callback)', function(t) {
+  beardo(dir)
+  .render('random-text.mustache', function(err, output) {
+    t.error(err)
+    t.equal(output, 'blah blah \n')
+    t.end()
+  })
+})
+
+test.skip('b.render(name, callback)', function(t) {
+  beardo(dir)
+  .render('random-text', function(err, output) {
+    t.error(err)
+    t.equal(output, 'blah blah \n')
+    t.end()
+  })
+})
